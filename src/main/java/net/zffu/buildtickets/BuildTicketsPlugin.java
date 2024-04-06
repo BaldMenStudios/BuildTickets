95f72206caa779832780797062aead5f698d3485
Zffu <radi0o0@outlook.com>
2024-04-06 20:36:34 +0200

    Added lombok Added basic config

--- src/main/java/net/zffu/buildtickets/BuildTicketsPlugin.java
+++ src/main/java/net/zffu/buildtickets/BuildTicketsPlugin.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BuildTicketsPlugin extends JavaPlugin {
 
+
+    private static BuildTicketsPlugin INSTANCE;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        INSTANCE = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static BuildTicketsPlugin getInstance() {
+        return INSTANCE;
+    }
+
 }
