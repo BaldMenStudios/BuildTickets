abc12d9aaa3bdf695d6366630b338685671baa51
Zffu <radi0o0@outlook.com>
2024-04-07 19:49:28 +0200

    Made a permissions enum for it to be easier to use permissions

--- src/main/java/net/zffu/buildtickets/config/Permissions.java
+++ src/main/java/net/zffu/buildtickets/config/Permissions.java
@@ -1,8 +1,52 @@
 package net.zffu.buildtickets.config;
 
-public class Permissions {
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
 
-    // Ticket Permissions
-    public static String CREATE_TICKET;
+@Getter
+public enum Permissions {
+
+    OPEN_TICKET_GUI("open-ticket-gui", "buildtickets.tickets.gui"),
+    CREATE_TICKET("create-ticket", "buildtickets.tickets.create"),
+    CHANGE_TICKET_PRIORITY("change-ticket-priority", "buildtickets.tickets", "change_priority"),
+    JOIN_TICKET("join-ticket", "buildtickets.tickets", "join"),
+    JOIN_TICKET_WITHOUT_HELP("join-without-help", "buildtickets.tickets", "join_without_help"),
+    REQUEST_HELP("request_help", "buildtickets.tickets", "request_help"),
+    TICKET_COMPLETE("ticket-mark-as-complete", "buildtickets.tickets", "complete.mark"),
+    TICKET_COMPLETE_CONFIRM("ticket-confirm-as-complete", "buildtickets.tickets", "complete.confirm"),
+    TICKET_CHANGE_REASON("ticket-change-reason", "buildtickets.tickets", "change_reason"),
+    LEAVE_TICKET("ticket-leave", "buildtickets.tickets", "leave"),
+    TICKET_VIEWER("open-ticket-viewer", "buildtickets.viewer", "open"),
+
+    OPEN_TICKET_PANEL("open-ticket-panel", "buildtickets.panel.open"),
+    PANEL_PLAYER_STATS("player-stats-panel-permission", "buildtickets.panel.player_stats"),
+    PANEL_ACTIVE_TICKETS("active-tickets-panel", "buildtickets.panel.active_tickets");
+
+    private String permission;
+    private String otherPermission;
+    private String configKey;
+
+    private Permissions(String configId, String defaultPermission) {
+        this.configKey = configId;
+        this.permission = defaultPermission;
+        this.otherPermission = defaultPermission;
+    }
+
+    private Permissions(String configId, String permissionNode, String permissionSubNode) {
+        this.configKey = configId;
+        this.permission = permissionNode + "." + permissionSubNode;
+        this.otherPermission = permissionNode + ".other." + permissionSubNode;
+    }
+
+    public static void loadFromConfig(FileConfiguration configuration) {
+        for(Permissions permissions : Permissions.values()) {
+            if(configuration.contains(permissions.configKey + "-permission")) {
+                permissions.permission = configuration.getString(permissions.configKey + "-permission");
+            }
+            if(configuration.contains(permissions.configKey + "-other-permission")) {
+                permissions.otherPermission = configuration.getString(permissions.configKey + "-other-permission");
+            }
+        }
+    }
 
 }
