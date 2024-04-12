05450c6d2903217cc1cb2cb40ad38cf27feaaec0
Zffu <radi0o0@outlook.com>
2024-04-13 00:19:19 +0200

    Started migratining the /ticket command to locales

--- src/main/java/net/zffu/buildtickets/commands/TicketCommand.java
+++ src/main/java/net/zffu/buildtickets/commands/TicketCommand.java
@@ -1,9 +1,11 @@
 package net.zffu.buildtickets.commands;
 
+import jdk.vm.ci.meta.Local;
 import net.zffu.buildtickets.BuildTicketsPlugin;
 import net.zffu.buildtickets.config.Permissions;
 import net.zffu.buildtickets.gui.impl.TicketBrowserGUI;
 import net.zffu.buildtickets.config.Messages;
+import net.zffu.buildtickets.locale.LocaleManager;
 import net.zffu.buildtickets.tickets.BuildTicket;
 import net.zffu.buildtickets.tickets.TicketPriority;
 import org.bukkit.command.Command;
@@ -22,7 +24,7 @@ public class TicketCommand implements CommandExecutor {
 
         if(args.length == 0) {
             if(!Permissions.OPEN_TICKET_GUI.hasPermission(player)) {
-                player.sendMessage(Messages.NO_PERMISSION.getMessage());
+                player.sendMessage(LocaleManager.getMessage("permission.not_met"));
                 return false;
             }
             new TicketBrowserGUI(0).open(player);
@@ -33,12 +35,12 @@ public class TicketCommand implements CommandExecutor {
 
         if(sub.equals("create")) {
             if(args.length < 2) {
-                player.sendMessage(Messages.INVALID_USAGE.getMessage());
+                player.sendMessage(LocaleManager.getMessage("permission.not_met"));
                 return false;
             }
 
             if(!Permissions.CREATE_TICKET.hasPermission(player)) {
-                player.sendMessage(Messages.NO_PERMISSION.getMessage());
+                player.sendMessage(LocaleManager.getMessage("permission.not_met"));
                 return false;
             }
 
@@ -47,7 +49,7 @@ public class TicketCommand implements CommandExecutor {
             BuildTicket buildTicket = new BuildTicket(reason, (args.length >= 3 ? TicketPriority.getValue(args[2]) : TicketPriority.NORMAL), player.getUniqueId());
             BuildTicketsPlugin.getInstance().getTickets().add(buildTicket);
             BuildTicketsPlugin.getInstance().getOrCreateBuilder(player.getUniqueId()).createTicket();
-            player.sendMessage(Messages.TICKET_CREATED.getMessage());
+            player.sendMessage(LocaleManager.getMessage("ticket.created"));
         }
         else {
             player.sendMessage("§7------ §a§lBuild Tickets §r§7------");
