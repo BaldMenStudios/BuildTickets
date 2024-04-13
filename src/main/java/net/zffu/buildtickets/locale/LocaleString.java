56be9aff755a7b7d68872ba91f550ce6007b187f
Zffu <radi0o0@outlook.com>
2024-04-13 09:40:04 +0200

    Added enum to save up memory for each locale

---
+++ src/main/java/net/zffu/buildtickets/locale/LocaleString.java
@@ -0,0 +1,50 @@
+package net.zffu.buildtickets.locale;
+
+/**
+ * Enum to store locale keys, seems useless but is actually a bit useful and allows us to use EnumMap for free optimization so.
+ */
+public enum LocaleString {
+
+    PERMISSION_NOT_MET("permission.not_met"),
+    USAGE_MISSING_ARGS("usage.missing_arguments"),
+
+    PAGE_ALREADY_LAST("page.already.last"),
+    PAGE_ALREADY_FIRST("page.already.first"),
+
+    PROMPT_ENTER("prompt.enter"),
+
+    TICKET_NOTE_ADDED("ticket.note.added"),
+    TICKET_NOTE_EDIT("ticket.note.edit"),
+
+    TICKET_CREATED("ticket.created"),
+    TICKET_JOIN_HELP_DISABLED("ticket.join.help.disabled"),
+    TICKET_JOIN("ticket.join"),
+    TICKET_JOIN_ALREADY("ticket.join.already"),
+    TICKET_BUILDER_NOT("ticket.builder.not"),
+    TICKET_QUIT("ticket.quit"),
+
+    TICKET_PRIORITY_ALREADY("ticket.priority.already"),
+    TICKET_PRIORITY_CHANGE("ticket.priority.change"),
+
+    TICKET_HELP_ON("ticket.help.on"),
+    TICKET_HELP_OFF("ticket.help.off"),
+
+    TICKET_COMPLETION_WAITING("ticket.completion.waiting"),
+    TICKET_COMPLETION_CONFIRMED("ticket.completion.confirmed"),
+
+    BUILDMODE_ON("buildmode.on"),
+    BUILDMODE_OFF("buildmode.off"),
+
+    BUILDPHYSICS_ON("buildphysics.on"),
+    BUILDPHYSICS_OFF("buildphysics.off");
+
+    private String key;
+
+    private LocaleString(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
