af80285baf2564d6ffe2c513ccde0087629f0867
Zffu <103074097+Radi0o@users.noreply.github.com>
2024-04-15 19:53:41 +0200

    Added javadoc

--- api/src/main/java/net/zffu/buildtickets/api/data/ITicket.java
+++ api/src/main/java/net/zffu/buildtickets/api/data/ITicket.java
@@ -1,7 +1,6 @@
 package net.zffu.buildtickets.api.data;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -9,18 +8,53 @@ import java.util.UUID;
  */
 public interface ITicket {
 
+    /**
+     * Gets the ticket's internal UUID.
+     * @return
+     */
     UUID getTicketUUID();
+
+    /**
+     * Gets the reason of the ticket.
+     * @return
+     */
     String getTicketReason();
+
     //todo: add priority
 
+    /**
+     * Gets the ticket creator's uuid.
+     * @return
+     */
     UUID getCreator();
 
+    /**
+     * Gets the builder that are working on the ticket.
+     * @return
+     */
     Collection<UUID> getBuilders();
 
+    /**
+     * Gets the note of the provided player's uuid.
+     * @param playerUUID
+     * @return
+     */
     String getNote(UUID playerUUID);
 
+    /**
+     * Returns true or false depending on the help status of the ticket.
+     * @return
+     */
     boolean isNeedingHelp();
 
+    /**
+     * <pReturns the completion state of the ticket.</p>
+     * <p>Current Possible States:</p>
+     * <p>- -1: Not waiting for confirmation</p>
+     * <p>- 0: Waiting for confirmation</p>
+     * <p>- 1: Is Completed</p>
+     * @return
+     */
     int getCompletionState();
 }
 
