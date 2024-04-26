76c9c2dcd1bc8616a6296c532963206489b23cb3
Zffu <radi0o0@outlook.com>
2024-04-26 22:33:11 +0200

    Converted TicketNotesGUI to new paginated framework

--- src/main/java/net/zffu/buildtickets/gui/impl/TicketNotesGUI.java
+++ src/main/java/net/zffu/buildtickets/gui/impl/TicketNotesGUI.java
@@ -2,22 +2,22 @@ package net.zffu.buildtickets.gui.impl;
 
 import dev.triumphteam.gui.guis.GuiItem;
 import net.zffu.buildtickets.BuildTicketsPlugin;
+import net.zffu.buildtickets.gui.ItemConvertible;
 import net.zffu.buildtickets.gui.PaginatedGUI;
 import net.zffu.buildtickets.tickets.BuildTicket;
 import net.zffu.buildtickets.utils.HeadUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Item;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class TicketNotesGUI extends PaginatedGUI {
+public class TicketNotesGUI extends PaginatedGUI<TicketNotesGUI.Notes> {
     private BuildTicket ticket;
 
     public TicketNotesGUI(BuildTicket ticket, int page) {
@@ -57,22 +57,8 @@ public class TicketNotesGUI extends PaginatedGUI {
     }
 
     @Override
-    public List<ItemStack> getStacks() {
-        List<ItemStack> stacks = new ArrayList<>();
-
-        for(UUID posterUUID : ticket.getNoteCreators()) {
-            OfflinePlayer poster = Bukkit.getOfflinePlayer(posterUUID);
-            String note = ticket.getNotes().get(posterUUID);
-
-            ItemStack stack = HeadUtils.getHeadStack(posterUUID);
-            ItemMeta meta = stack.getItemMeta();
-            meta.setDisplayName("§a" + poster.getPlayer().getName() + "'s Note");
-            meta.setLore(Arrays.asList("§7Note: §f" + note));
-
-            stack.setItemMeta(meta);
-            stacks.add(stack);
-        }
-        return stacks;
+    public List<Notes> getElements() {
+        return ticket.getNotes().entrySet().stream().map(entry -> new Notes(entry)).collect(Collectors.toList());
     }
 
     @Override
@@ -82,4 +68,29 @@ public class TicketNotesGUI extends PaginatedGUI {
 
     @Override
     public void handleMenu(InventoryClickEvent event) {}
+
+    public class Notes implements ItemConvertible {
+
+        private Map.Entry<UUID, String> entry;
+
+        public Notes(Map.Entry<UUID, String> entry) {
+            this.entry = entry;
+        }
+
+        @Override
+        public ItemStack toItemStack() {
+            OfflinePlayer poster = Bukkit.getOfflinePlayer(entry.getKey());
+            String note = entry.getValue();
+
+            ItemStack stack = HeadUtils.getHeadStack(entry.getKey());
+            ItemMeta meta = stack.getItemMeta();
+            meta.setDisplayName("§a" + poster.getPlayer().getName() + "'s Note");
+            meta.setLore(Arrays.asList("§7Note: §f" + note));
+
+            stack.setItemMeta(meta);
+
+            return stack;
+        }
+    }
+
 }
